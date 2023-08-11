a6f5bc433f81b5d1cc88a5dcb88e844f5fa7b2a0
saadDevlop <105066118+saadDevlop@users.noreply.github.com>
2023-08-11 14:36:25 +0100

    add side nav bar 2

---
+++ stageTemplates/src/app/sidenav/nav-data.ts
@@ -0,0 +1,23 @@
+export const navbarData = [
+    {
+        routelink: 'site',
+        icon : 'fal fa-home',
+        label: 'Site'
+    },
+    {
+        routelink: 'grandeur',
+        icon : 'fal fa-box',
+        label: 'Grandeur'
+    },
+    {
+        routelink: 'crudUser',
+        icon : 'fal fa-user',
+        label: 'Utilisateur'
+    },
+    {
+        routelink: 'settings',
+        icon : 'fal fa-cog',
+        label: 'parametres'
+    },
+
+];
